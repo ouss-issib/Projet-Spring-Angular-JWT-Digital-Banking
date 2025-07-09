caa45637a4bdc98fa709703ad2eee38537e4698b
ouss-issib <oussamaissib@gmail.com>
2025-07-09 11:19:20 +0100

    feat: implement home component

--- ebanking-frontend/src/app/components/home/home.component.ts
+++ ebanking-frontend/src/app/components/home/home.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  username: string = '';
+  role: string = '';
+  authService = inject(AuthService);
 
+  ngOnInit() {
+    this.username = this.authService.username || 'Guest';
+    this.role = this.authService.roles.includes('ADMIN') ? 'ADMIN' : 'USER';
+  }
 }
